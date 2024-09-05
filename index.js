32dcd4776fb0fc3f504212916faf1c99c933fed9
Lyall <me@lyall.lol>
2024-09-05 09:51:45 +0100

    ip

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ server.get("/", (req, res) => {
     res.json({ alive: true, started: startDate.toUTCString(), message: "I'm alive!!!" });
 });
 
+server.get("/ip", (req, res) => {
+    res.json({
+        ip: req.socket.remoteAddress.split("::ffff:")[1] || req.socket.remoteAddress,
+        realIp: req.headers[decodeURIComponent((req.query.header || "CF-Connecting-IP").toLowerCase())] || null
+    });
+});
+
 server.get("/headers", (req, res) => {
     res.json(req.headers);
 });
